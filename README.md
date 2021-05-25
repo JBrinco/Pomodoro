726d598f5cf3d37826c5185f4dcc93d9db3306d5
JBrinco <j.brinco@campus.fct.unl.pt>
2021-05-25 19:21:49 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Pomodoro
+
 Simple Pomodoro timer for the command line
+
+# What it is
+
+A simple Bash script for tracking your pomodoros! Will write a log to ~/.pomodorolog. You need to put the notification.ogg file in ~/.config
+
+# To add
+
+- [ ] .config/pomodoro directory with log, config file, etc, as well as a better way to acess the log and count pomodoro time.
+
+
+Adapted from https://github.com/rukshn/pomodoro
---
+++ pomodoro.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# Simple pomodoro timer for Linux
+
+RED='\033[0;31m'
+ORANGE='\033[0;33m'
+
+if [[ "$1" == "-v" ]] || [[ "$1" == "--version" ]]; then
+	echo -e "${ORANGE}POMODORO TIMER"
+	echo "  v: 1.0"
+	echo "  Adapted from: rukshn.github.io"
+	echo -e "  email: mail[@]jbrinco.com"
+	return
+fi
+
+TIMER=3000
+BREAKTIMER=600
+NUMBER=3
+
+
+while getopts ":d:b:n:h" o; do case "${o}" in
+	d) TIMER=$((OPTARG*60)) ;;
+	b) BREAKTIMER=$((OPTARG*60)) ;;
+	n) NUMBER=${OPTARG} ;;
+	h) echo -e "Usage: pomodoro (50 minute cycle default)\n Options: \n d: timer duration in minutes (default 50)\n b: Break duration in minutes (default 10)\n n: Number of pomodoros (default 3)" && exit 0 ;;
+	*) printf "Invalid option: -%s\\n" "$OPTARG" && exit 1 ;;
+esac done
+
+
+((MINTIMER=TIMER/60))
+
+
+while [ "$NUMBER" -gt 1 ]
+do
+	TIMELEFT=$TIMER
+	clear && echo -e "${RED}Timer set for $((TIMER/60)) minutes\n${NUMBER} Pomodoros left "
+	while [ "$TIMELEFT" -gt 60 ]
+	do
+		((TIMELEFT=TIMELEFT-60)); sleep 60 && clear && echo -e "Work Period\n$((TIMELEFT/60)) minutes left\n${NUMBER} Pomodoros to go";
+	done
+
+	echo "$(date "+%d %b %Y 🕑 %H:%M") $MINTIMER minutes"  >> ~/.pomodorolog
+	notify-send 'Pomodoro over!' && paplay ~/.config/notification.ogg
+
+
+	clear && read -p "Pomdoro over. Press enter to start break timer "
+
+
+	((NUMBER=NUMBER-1))
+	TIMELEFT=$BREAKTIMER
+	clear && echo -e "Break Period\n$((TIMELEFT/60)) minutes left\n${NUMBER} Pomodoros to go"
+	while [ "$TIMELEFT" -gt 60 ]
+	do
+		((TIMELEFT=TIMELEFT-60)); sleep 60 && clear && echo -e "Break Period\n$((TIMELEFT/60)) minutes left\n${NUMBER} Pomodoros to go"
+	done
+
+	notify-send 'Break time over!' && paplay ~/.config/notification.ogg
+
+	clear && read -p "Break time over. Press enter to start new Pomodoro "
+done
+echo "All done! Have a great day!"
