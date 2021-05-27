2e39a09653421b6f34c92fea9a91032b1c035351
JBrinco <j.brinco@campus.fct.unl.pt>
2021-05-27 18:20:55 +0100

    Bug Fix

--- pomodoro.sh
+++ pomodoro.sh
@@ -10,7 +10,7 @@ while getopts ":d:b:n:h" o; do case "${o}" in
 	d) TIMER=$((OPTARG*60)) ;;
 	b) BREAKTIMER=$((OPTARG*60)) ;;
 	n) NUMBER=${OPTARG} ;;
-	h) echo -e "Usage: pomodoro (50 minute cycle default)\n Options: \n d: timer duration in minutes (default 50)\n b: Break duration in minutes (default 10)\n n: Number of pomodoros (default 3)" && exit 0 ;;
+	h) echo -e "Usage: pomodoro \nOptions: \n d: timer duration in minutes (default 50)\n b: Break duration in minutes (default 10)\n n: Number of pomodoros (default 3)" && exit 0 ;;
 	*) printf "Invalid option: -%s\\n" "$OPTARG" && exit 1 ;;
 esac done
 
@@ -18,11 +18,11 @@ esac done
 ((MINTIMER=TIMER/60))
 
 
-while [ "$NUMBER" -gt 1 ]
+while [ "$NUMBER" -gt 0 ]
 do
 	TIMELEFT=$TIMER
 	clear && echo -e "Timer set for $((TIMER/60)) minutes\n${NUMBER} Pomodoros left "
-	while [ "$TIMELEFT" -gt 60 ]
+	while [ "$TIMELEFT" -gt 0 ]
 	do
 		((TIMELEFT=TIMELEFT-60)); sleep 60 && clear && echo -e "Work Period\n$((TIMELEFT/60)) minutes left\n${NUMBER} Pomodoros to go";
 	done
@@ -37,7 +37,7 @@ do
 	((NUMBER=NUMBER-1))
 	TIMELEFT=$BREAKTIMER
 	clear && echo -e "Break Period\n$((TIMELEFT/60)) minutes left\n${NUMBER} Pomodoros to go"
-	while [ "$TIMELEFT" -gt 60 ]
+	while [ "$TIMELEFT" -gt 0 ]
 	do
 		((TIMELEFT=TIMELEFT-60)); sleep 60 && clear && echo -e "Break Period\n$((TIMELEFT/60)) minutes left\n${NUMBER} Pomodoros to go"
 	done
