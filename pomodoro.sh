b94cc76c16babdea2cba26a58f05ceb7913e3f1f
JBrinco <j.brinco@campus.fct.unl.pt>
2021-06-14 16:11:26 +0100

    New log option. Moved stuff to .config/pomodoro

--- pomodoro.sh
+++ pomodoro.sh
@@ -6,11 +6,13 @@ TIMER=3000
 BREAKTIMER=600
 NUMBER=3
 
-while getopts ":d:b:n:h" o; do case "${o}" in
+while getopts ":d:b:l:L:n:h" o; do case "${o}" in
 	d) TIMER=$((OPTARG*60)) ;;
 	b) BREAKTIMER=$((OPTARG*60)) ;;
 	n) NUMBER=${OPTARG} ;;
-	h) echo -e "Usage: pomodoro \nOptions: \n d: timer duration in minutes (default 50)\n b: Break duration in minutes (default 10)\n n: Number of pomodoros (default 3)" && exit 0 ;;
+	l) echo -e "Last 10 pomodoros:\n" && tail ~/.config/pomodoro/pomodorolog && exit 0 ;;
+	L) echo -e "Full Pomodoro log:\n" && less ~/.config/pomodoro/pomodorolog ;;
+	h) echo -e "Usage: pomodoro \nOptions: \n d: timer duration in minutes (default 50)\n b: Break duration in minutes (default 10)\n n: Number of pomodoros (default 3)\n l: Show log of 10 last pomodoros\n L: Show full log" && exit 0 ;;
 	*) printf "Invalid option: -%s\\n" "$OPTARG" && exit 1 ;;
 esac done
 
@@ -27,8 +29,8 @@ do
 		((TIMELEFT=TIMELEFT-60)); sleep 60 && clear && echo -e "Work Period\n$((TIMELEFT/60)) minutes left\n${NUMBER} Pomodoros to go";
 	done
 
-	echo "$(date "+%d %b %Y 🕑 %H:%M") $MINTIMER minutes"  >> ~/.pomodorolog
-	notify-send 'Pomodoro over!' && paplay ~/.config/notification.ogg
+	echo "$(date "+%d %b %Y 🕑 %H:%M") $MINTIMER minutes"  >> ~/.config/pomodoro/pomodorolog
+	notify-send 'Pomodoro over!' && paplay ~/.config/pomodoro/notification.ogg
 
 
 	clear && read -p "Pomdoro over. Press enter to start break timer "
@@ -42,7 +44,7 @@ do
 		((TIMELEFT=TIMELEFT-60)); sleep 60 && clear && echo -e "Break Period\n$((TIMELEFT/60)) minutes left\n${NUMBER} Pomodoros to go"
 	done
 
-	notify-send 'Break time over!' && paplay ~/.config/notification.ogg
+	notify-send 'Break time over!' && paplay ~/.config/pomodoro/notification.ogg
 
 	clear && read -p "Break time over. Press enter to start new Pomodoro "
 done
